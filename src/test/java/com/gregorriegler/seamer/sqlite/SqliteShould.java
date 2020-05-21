4ab82698a92584404448433a661ae1f7063a1ad3
Gregor Riegler <gregor.riegler@ebcont.com>
2020-05-21 17:49:40 +0200

    refactor: extract duplication

--- src/test/java/com/gregorriegler/seamer/sqlite/SqliteShould.java
+++ src/test/java/com/gregorriegler/seamer/sqlite/SqliteShould.java
@@ -19,32 +19,30 @@ public class SqliteShould {
 
         Connection connection = createSqliteConnection("jdbc:sqlite::memory:");
 
-        try {
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-            statement.executeUpdate("drop table if exists seams");
-            statement.executeUpdate("create table seams (name string)");
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
+        createSchema(connection);
 
         try {
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
+            Statement statement = createStatement(connection);
             statement.executeUpdate("insert into seams values('expected seam name')");
+        } catch (SQLException e) {
+            handleError(e);
+        }
+
+        try{
+            Statement statement = createStatement(connection);
             ResultSet result = statement.executeQuery("select * from seams");
             while (result.next()) {
                 actualSeam = result.getString("name");
             }
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
+            handleError(e);
         }
 
         try {
             if (connection != null)
                 connection.close();
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
+            handleError(e);
         }
 
         assertThat(actualSeam).isEqualTo(expectedSeam);
@@ -55,8 +53,28 @@ public class SqliteShould {
         try {
             connection = DriverManager.getConnection(url);
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
+            handleError(e);
         }
         return connection;
+    }
+
+    private void createSchema(Connection connection) {
+        try {
+            Statement statement = createStatement(connection);
+            statement.executeUpdate("drop table if exists seams");
+            statement.executeUpdate("create table seams (name string)");
+        } catch (SQLException e) {
+            handleError(e);
+        }
+    }
+
+    private Statement createStatement(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);
+        return statement;
+    }
+
+    private void handleError(SQLException e) {
+        System.err.println(e.getMessage());
     }
 }
