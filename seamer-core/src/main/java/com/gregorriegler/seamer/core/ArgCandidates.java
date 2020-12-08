7e7e4ced0a8b18b70f47745560202bd97e788f7b
Gregor Riegler <gregor.riegler@ebcont.com>
2020-12-08 21:10:09 +0100

    R no need for apache-commons really

--- seamer-core/src/main/java/com/gregorriegler/seamer/core/ArgCandidates.java
+++ seamer-core/src/main/java/com/gregorriegler/seamer/core/ArgCandidates.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import static org.apache.commons.lang3.ArrayUtils.addAll;
-
 public class ArgCandidates implements Serializable {
     private final Map<Integer, List<Object>> argCandidates = new HashMap<>();
 
@@ -26,18 +24,19 @@ public class ArgCandidates implements Serializable {
 
     public List<Object[]> shuffle() {
         List<Object[]> argCombinations = new ArrayList<>();
-        loopOverPosition(0, new Object[]{}, argCombinations);
+        loopOverPosition(0, new ArrayList<>(), argCombinations);
         return argCombinations;
     }
 
-    private void loopOverPosition(int pos, Object[] leftArgs, List<Object[]> argCombinations) {
+    private void loopOverPosition(int pos, List<Object> leftArgs, List<Object[]> argCombinations) {
         for (int i = 0; i < argCandidates.get(pos).size(); i++) {
-            Object arg = argCandidates.get(pos).get(i);
+            ArrayList<Object> objects = new ArrayList<>(leftArgs);
+            objects.add(argCandidates.get(pos).get(i));
 
             if (onLastPosition(pos)) {
-                argCombinations.add(addAll(leftArgs, arg));
+                argCombinations.add(objects.toArray(new Object[0]));
             } else {
-                loopOverPosition(pos + 1, addAll(leftArgs, arg), argCombinations);
+                loopOverPosition(pos + 1, objects, argCombinations);
             }
         }
     }
